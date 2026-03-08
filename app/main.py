ff7b16a1b8681d133a57521821f448ce89548f62
Holygrizzly <holygrizzly@gmail.com>
2026-03-08 23:43:59 +0700

    Add proof and wallet routers

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from .compute import router as compute_router
 from .jobs import router as jobs_router
 from .payment import router as payment_router
 from .reputation import router as reputation_router
+from .proof import router as proof_router
+from .wallet import router as wallet_router
 
 
 app = FastAPI(
@@ -16,6 +18,8 @@ app.include_router(payment_router)
 app.include_router(compute_router)
 app.include_router(jobs_router)
 app.include_router(reputation_router)
+app.include_router(proof_router)
+app.include_router(wallet_router)
 
 
 @app.get("/")
