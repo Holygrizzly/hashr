7615717fe071af634b4a517f747fcc46cc8baec6
Holygrizzly <holygrizzly@gmail.com>
2026-03-11 14:52:12 +0700

    Add node manifest endpoint

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from .reputation import router as reputation_router
 from .proof import router as proof_router
 from .wallet import router as wallet_router
 from .identity import router as identity_router
-
+from .manifest import router as manifest_router
 
 app = FastAPI(
     title="hashr",
@@ -22,6 +22,7 @@ app.include_router(reputation_router)
 app.include_router(proof_router)
 app.include_router(wallet_router)
 app.include_router(identity_router)
+app.include_router(manifest_router)
 
 
 @app.get("/")
