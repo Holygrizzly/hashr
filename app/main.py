e1d1d29b505da623ca00b6d9b52a46407327ac5e
Holygrizzly <holygrizzly@gmail.com>
2026-03-11 22:53:13 +0700

    Add payments router to FastAAdd payment metadata endpointPI application

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from .capabilities import router as capabilities_router
 from .pricing import router as pricing_router
 from .registry import router as registry_router
 from .handshake import router as handshake_router
+from .payments import router as payments_router
 
 
 app = FastAPI(
@@ -35,6 +36,7 @@ app.include_router(capabilities_router)
 app.include_router(pricing_router)
 app.include_router(registry_router)
 app.include_router(handshake_router)
+app.include_router(payments_router)
 
 
 @app.get("/")
